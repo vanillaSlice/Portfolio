d1331499606b095c419f327020b0ce4d599093d2
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-05-11 22:16:55 +0100

    Completed About section

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,39 @@
-(function () {
+$(document).ready(function () {
   
   'use strict';
   
-  var quotes = ["\"like a night club in the morning...\"",
-              "\"like a recently disinfected shithouse...\"",
-              "\"like a dose of scabies...\"",
-              "\"like a death at a birthday party...\"",
-              "\"like a sucked and spat-out smartie...\"",
-              "\"like a black widow spider in the shadows of disgrace...\""];
-  
-  $(document).ready(function () {
+  // navigation link handler
+  $('.navbar-collapse a').click(function (event) {
+    event.preventDefault();
     
-    $('.navbar-collapse a').click(function (event) {
-      // for smooth scrolling
-      event.preventDefault();
-      var top = $(event.target.hash).offset().top - 50, duration = 800;
-      $('html, body').animate({scrollTop: top}, duration);
+    // for smooth scrolling
+    var top = $(event.target.hash).offset().top - 50;
+    var duration = 800;
+    $('html, body').animate({scrollTop: top}, duration);
         
-      // will make menu disappear when nav clicked
-      if ($('button').is(':visible')) {
-        $('button').trigger('click');
-      }
-    });
-    
-    // show random quote
-    var quote = quotes[Math.round(Math.random() * (quotes.length - 1))];
-    $("#quote").text(quote);
-    
+    // will make menu disappear when a nav link is clicked
+    var menu_button = $('.navbar-toggle');
+    if (menu_button.is(':visible')) {
+      menu_button.trigger('click');
+    }
   });
   
-}());
+  // set the random quote
+  var quotes = ['like a night club in the morning...',
+        'like a recently disinfected shithouse...',
+        'like a dose of scabies...',
+        'like a death at a birthday party...',
+        'like a sucked and spat-out smartie...',
+        'like a black widow spider in the shadows of disgrace...'];
+  var quote = quotes[Math.round(Math.random() * (quotes.length - 1))];
+  $('#quote').text(quote);
+  
+  // change logo image on hover
+  $('.logo').hover(function (event) {
+    var src = event.target.src;
+    var alt_src = event.target.getAttribute('data-alt-src');
+    event.target.setAttribute('src', alt_src);
+    event.target.setAttribute('data-alt-src', src);
+  });
+  
+});
