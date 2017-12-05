e0e3aab72f32db160e9aeac2bf2251cc2dd681d2
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-12-05 21:10:22 +0000

    Tidy up

--- main.js
+++ main.js
@@ -1,38 +1,41 @@
 $(document).ready(function () {
-  
+
   'use strict';
-  
+
   $('.scrollable').click(function (event) {
     event.preventDefault();
-    
+
     // for smooth scrolling
     var top = $(event.target.hash).offset().top - 50;
     var duration = 800;
-    $('html, body').animate({scrollTop: top}, duration);
-        
+    $('html, body').animate({
+      scrollTop: top
+    }, duration);
+
     // will make menu disappear when a nav link is clicked
-    var menu_button = $('.navbar-toggle');
-    if (menu_button.is(':visible') && !menu_button.hasClass('collapsed')) {
-      menu_button.trigger('click');
+    var menuBtn = $('.navbar-toggle');
+    if (menuBtn.is(':visible') && !menuBtn.hasClass('collapsed')) {
+      menuBtn.trigger('click');
     }
   });
-  
+
   // set the random quote
   var quotes = ['like a night club in the morning...',
-        'like a recently disinfected shithouse...',
-        'like a dose of scabies...',
-        'like a death at a birthday party...',
-        'like a sucked and spat-out smartie...',
-        'like a black widow spider in the shadows of disgrace...'];
+    'like a recently disinfected shithouse...',
+    'like a dose of scabies...',
+    'like a death at a birthday party...',
+    'like a sucked and spat-out smartie...',
+    'like a black widow spider in the shadows of disgrace...'
+  ];
   var quote = quotes[Math.round(Math.random() * (quotes.length - 1))];
-  $('#quote').text(quote);
-  
+  $('.quote').text(quote);
+
   // change logo image on hover
-  $('#logo').hover(function (event) {
+  $('.logo').hover(function (event) {
     var src = event.target.src;
-    var alt_src = event.target.getAttribute('data-alt-src');
-    event.target.setAttribute('src', alt_src);
+    var altSrc = event.target.getAttribute('data-alt-src');
+    event.target.setAttribute('src', altSrc);
     event.target.setAttribute('data-alt-src', src);
   });
-  
+
 });
