998ad671a8256c4340a174af86f6a747dad67531
Mike Lowe <mikelowedev@gmail.com>
2019-02-10 11:37:19 +0000

    Refactoring

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,41 +1,36 @@
-$(document).ready(function () {
+import logo from '../images/logo.png';
+import altLogo from '../images/colour-logo.jpg';
 
-  'use strict';
+$('.scrollable').click((event) => {
+  event.preventDefault();
 
-  $('.scrollable').click(function (event) {
-    event.preventDefault();
+  // for smooth scrolling
+  const top = $(event.target.hash).offset().top - 50;
+  const duration = 800;
+  $('html, body').animate({
+    scrollTop: top,
+  }, duration);
 
-    // for smooth scrolling
-    var top = $(event.target.hash).offset().top - 50;
-    var duration = 800;
-    $('html, body').animate({
-      scrollTop: top
-    }, duration);
-
-    // will make menu disappear when a nav link is clicked
-    var menuBtn = $('.navbar-toggle');
-    if (menuBtn.is(':visible') && !menuBtn.hasClass('collapsed')) {
-      menuBtn.trigger('click');
-    }
-  });
-
-  // set the random quote
-  var quotes = ['like a night club in the morning...',
-    'like a recently disinfected shithouse...',
-    'like a dose of scabies...',
-    'like a death at a birthday party...',
-    'like a sucked and spat-out smartie...',
-    'like a black widow spider in the shadows of disgrace...'
-  ];
-  var quote = quotes[Math.round(Math.random() * (quotes.length - 1))];
-  $('.quote').text(quote);
-
-  // change logo image on hover
-  $('.logo').hover(function (event) {
-    var src = event.target.src;
-    var altSrc = event.target.getAttribute('data-alt-src');
-    event.target.setAttribute('src', altSrc);
-    event.target.setAttribute('data-alt-src', src);
-  });
-
+  // will make menu disappear when a nav link is clicked
+  const menuBtn = $('.navbar-toggle');
+  if (menuBtn.is(':visible') && !menuBtn.hasClass('collapsed')) {
+    menuBtn.trigger('click');
+  }
 });
+
+// set the random quote
+const quotes = ['like a night club in the morning...',
+  'like a recently disinfected shithouse...',
+  'like a dose of scabies...',
+  'like a death at a birthday party...',
+  'like a sucked and spat-out smartie...',
+  'like a black widow spider in the shadows of disgrace...',
+];
+const quote = quotes[Math.round(Math.random() * (quotes.length - 1))];
+$('.quote').text(quote);
+
+// change logo image on hover
+$('.logo').hover(
+  e => e.target.setAttribute('src', altLogo),
+  e => e.target.setAttribute('src', logo),
+);
