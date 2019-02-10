74d3d4b964903d5f9ee941e9b1ac8076ac03273c
Mike Lowe <mikelowedev@gmail.com>
2019-02-10 13:28:02 +0000

    Refactoring

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,36 +1,59 @@
 import logo from '../images/logo.png';
 import altLogo from '../images/colour-logo.jpg';
 
-$('.scrollable').click((event) => {
-  event.preventDefault();
+/*
+ * Constants
+ */
 
-  // for smooth scrolling
-  const top = $(event.target.hash).offset().top - 50;
-  const duration = 800;
-  $('html, body').animate({
-    scrollTop: top,
-  }, duration);
-
-  // will make menu disappear when a nav link is clicked
-  const menuBtn = $('.navbar-toggle');
-  if (menuBtn.is(':visible') && !menuBtn.hasClass('collapsed')) {
-    menuBtn.trigger('click');
-  }
-});
-
-// set the random quote
-const quotes = ['like a night club in the morning...',
+const quotes = [
+  'like a night club in the morning...',
   'like a recently disinfected shithouse...',
   'like a dose of scabies...',
   'like a death at a birthday party...',
   'like a sucked and spat-out smartie...',
   'like a black widow spider in the shadows of disgrace...',
 ];
-const quote = quotes[Math.round(Math.random() * (quotes.length - 1))];
-$('.quote').text(quote);
+const quote = quotes[Math.floor(Math.random() * quotes.length)];
 
-// change logo image on hover
-$('.logo').hover(
-  e => e.target.setAttribute('src', altLogo),
-  e => e.target.setAttribute('src', logo),
-);
+/*
+ * Elements
+ */
+
+const htmlElement = $('html');
+const bodyElement = $('body');
+const scrollableElements = $('.js-scrollable');
+const navbarToggleElement = $('.js-navbar-toggle');
+const quoteElement = $('.js-quote');
+const logoElement = $('.js-logo');
+
+/*
+ * Functions
+ */
+
+function handleScrollableElementClick(e) {
+  e.preventDefault();
+
+  const scrollTop = $(e.target.hash).offset().top - 50;
+  const duration = 800;
+  $(htmlElement, bodyElement).animate({ scrollTop }, duration);
+
+  if (navbarToggleElement.is(':visible') && !navbarToggleElement.hasClass('collapsed')) {
+    navbarToggleElement.trigger('click');
+  }
+}
+
+function handleLogoElementHoverIn(e) {
+  e.target.setAttribute('src', altLogo);
+}
+
+function handleLogoElementHoverOut(e) {
+  e.target.setAttribute('src', logo);
+}
+
+/*
+ * Initialise
+ */
+
+scrollableElements.click(handleScrollableElementClick);
+logoElement.hover(handleLogoElementHoverIn, handleLogoElementHoverOut);
+quoteElement.text(quote);
